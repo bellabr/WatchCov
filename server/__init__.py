7fcae78e7bdc494f2cb3d201aa1df0bd08d2e4ca
xiaoshe3 <shengyang.xiao@mail.utoronto.ca>
2021-03-13 16:17:50 -0500

    finished process age and process date

--- server/__init__.py
+++ server/__init__.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, request
+import json, datetime
 
 app = Flask(__name__, static_folder='../client', static_url_path='/')
 
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
+
+
+# return JSON case file
+@app.route('/api/cases', methods=["GET"])
+def send_cases():
+    with open('./client/cases/test.JSON') as f:
+        case = json.load(f)
+    return json.dumps(case)
+
+# process age request
+@app.route('/api/cases/age', methods=["GET"])
+def process_age():
+    startAge = request.args.get('startAge')
+    endAge = request.args.get('endAge')
+    return "Ages %s to %s" % (startAge, endAge)
+
+# process date request
+@app.route('/api/cases/date', methods=["GET"])
+def process_date():
+    startTime = request.args.get('startTime')
+    processedStart = datetime.datetime.fromisoformat(startTime[:-1])
+    endTime = request.args.get('endTime')
+    processedEnd = datetime.datetime.fromisoformat(endTime[:-1])
+    return "Time " + str(processedStart) + " to " + str(processedEnd)
