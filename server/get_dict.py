e85874e911f35de81f00abfdf580e58cee0dfc5f
Bella Bregazzi <arbregazzi@me.com>
2021-03-14 07:13:40 -0600

    get_dict.py update to include startDate and endDate

--- server/get_dict.py
+++ server/get_dict.py
@@ -1,16 +1,9 @@
 from typing import Dict
-from datetime import datetime
+from datetime import datetime, date
 import csv
 
 def load_dict(filename: str) -> Dict:
-    """Create a dictionary using <filename> in the format specified:
-
-    Dict['health_region': {'cases', 'latitude', 'longitude'}, ...]
-
-    Where 'cases' refers to the integer number of cases in 'health_region',
-    'latitude' refers to the float latitude of 'health_region', and
-    'longitude' refers to the float longitude of 'health_region',
-    as specified in <filename>.
+    """Create a global dictionary of <filename> containing only values in Canada.
     """
     d = {}
     id = 1
@@ -29,23 +22,27 @@ def load_dict(filename: str) -> Dict:
 
 gd = load_dict('server\COVID19_case_details.csv')
 
-def query(ageRange: str = None, gender: str = None) -> Dict:
-    """Create a dictionary using <gd> of the cases that fall into the category
-    <ageRange> and <gender>, if they are not None, in the format specified:
+def query(ageRange: str = None, gender: str = None, 
+startDate: datetime = None, endDate: datetime = None) -> Dict:
+    """Create a dictionary using <gd> of the cases that fall into the category <ageRange>, <gender>, 
+    <startDate>, <endDate>, if they are not None, in the format specified:
 
     Dict['health_region': {'cases', 'latitude', 'longitude'}, ...]
 
     Where 'cases' refers to the integer number of cases in 'health_region',
     'latitude' refers to the float latitude of 'health_region', and
-    'longitude' refers to the float longitude of 'health_region',
-    as specified in <filename>.
+    'longitude' refers to the float longitude of 'health_region'.
 
-    If <ageRange> or <gender> is None, there is no restriction on that category
-    and so all cases are returned in the dictionary.
+    If <ageRange>, <gender>, <startDate>, <endDate>, is None, there is no restriction on that category 
+    and so all cases of that category are returned in the dictionary.
+
+    Precondition: 
+        - if <startDate> is not None, then <endDate> is not None.
+        - if <startDate> and <endDate> are not None, then <startDate> <= <endDate>
     """
     q = {}
     for key in gd:
-        if _satisfies_condition(key, ageRange, gender):
+        if _satisfies_condition(key, ageRange, gender, startDate, endDate):
             if gd[key]['healthRegion'] not in q:
                 q[gd[key]['healthRegion']] = {'cases': 1, \
                     'latitude': float(gd[key]['latitude']), \
@@ -54,16 +51,17 @@ def query(ageRange: str = None, gender: str = None) -> Dict:
                 q[gd[key]['healthRegion']]['cases'] += 1
     return q
 
-def _satisfies_condition(key, ageRange, gender) -> bool:
+def _satisfies_condition(key, ageRange, gender, startDate, endDate) -> bool:
     """A helper function for query
     """
     if ageRange is None or gd[key]['ageRange'] == ageRange:
         if gender is None or gd[key]['gender'] == gender:
-            return True
+            if startDate is None or startDate <= gd[key]['dateReported'] <= endDate:
+                return True
     return False
 
 if __name__ == '__main__':
-    t = query(gender='Male', ageRange='80+')
+    t = query(gender='Female', ageRange='70-79', startDate=datetime(2020, 3, 11), endDate=datetime(2020, 3, 11))
 
     for key in t:
         print(f'{key}: {t[key]}')
