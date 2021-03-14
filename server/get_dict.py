b5da6da2a96d0ececc573055147e1d6e2771598e
Bella Bregazzi <arbregazzi@me.com>
2021-03-13 21:21:35 -0700

    get_dict.py global dictionary update

--- server/get_dict.py
+++ server/get_dict.py
@@ -1,4 +1,5 @@
 from typing import Dict
+from datetime import datetime
 import csv
 
 def load_dict(filename: str) -> Dict:
@@ -12,23 +13,25 @@ def load_dict(filename: str) -> Dict:
     as specified in <filename>.
     """
     d = {}
+    id = 1
     with open(filename, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             try:
-                if row['health_region'] not in d:
-                    d[row['health_region']] = {'cases': 1, \
-                        'latitude': float(row['latitude']), \
-                            'longitude': float(row['longitude'])}
-                else:
-                    d[row['health_region']]['cases'] += 1
+                d[id] = {'latitude': float(row['latitude']), 'longitude': float(row['longitude']), \
+                    'healthRegion': row['health_region'], 'ageRange': row['age_group'], 'gender': row['gender'], \
+                        'dateReported': datetime.strptime(row['date_reported'][:-3], '%Y/%m/%d %I:%M:%S'), \
+                            'exposure': row['exposure'], 'caseStatus': row['case_status'], 'province': row['province']} 
+                id += 1
             except ValueError:  # no longitude nor latitude
                 pass
     return d
 
-def load_dict_by_age(filename: str, age_group: str) -> Dict:
-    """Create a dictionary containing only data of the specified age category, <age_group>
-    in <filename> in the format specified:
+gd = load_dict('server\COVID19_case_details.csv')
+
+def query(ageRange: str = None, gender: str = None) -> Dict:
+    """Create a dictionary using <gd> of the cases that fall into the category
+    <ageRange> and <gender>, if they are not None, in the format specified:
 
     Dict['health_region': {'cases', 'latitude', 'longitude'}, ...]
 
@@ -37,54 +40,30 @@ def load_dict_by_age(filename: str, age_group: str) -> Dict:
     'longitude' refers to the float longitude of 'health_region',
     as specified in <filename>.
 
-    If <age_group> is not a specified key in <filename>, an empty dictionary is returned.
+    If <ageRange> or <gender> is None, there is no restriction on that category
+    and so all cases are returned in the dictionary.
     """
-    d = {}
-    with open(filename, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            try:
-                if row['age_group'] == age_group:
-                    if row['health_region'] not in d:
-                        d[row['health_region']] = {'cases': 1, \
-                            'latitude': float(row['latitude']), \
-                                'longitude': float(row['longitude'])}
-                    else:
-                        d[row['health_region']]['cases'] += 1
-            except ValueError:  # no longitude nor latitude
-                pass
-    return d
+    q = {}
+    for key in gd:
+        if _satisfies_condition(key, ageRange, gender):
+            if gd[key]['healthRegion'] not in q:
+                q[gd[key]['healthRegion']] = {'cases': 1, \
+                    'latitude': float(gd[key]['latitude']), \
+                        'longitude': float(gd[key]['longitude'])}
+            else:
+                q[gd[key]['healthRegion']]['cases'] += 1
+    return q
 
-def load_dict_by_gender(filename: str, gender: str) -> Dict:
-    """Create a dictionary containing only data of the specified age category, <gender>
-    in <filename> in the format specified:
-
-    Dict['health_region': {'cases', 'latitude', 'longitude'}, ...]
-
-    Where 'cases' refers to the integer number of cases in 'health_region',
-    'latitude' refers to the float latitude of 'health_region', and
-    'longitude' refers to the float longitude of 'health_region',
-    as specified in <filename>.
-
-    If <gender> is not a specified key in <filename>, an empty dictionary is returned.
+def _satisfies_condition(key, ageRange, gender) -> bool:
+    """A helper function for query
     """
-    d = {}
-    with open(filename, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            try:
-                if row['gender'] == gender:
-                    if row['health_region'] not in d:
-                        d[row['health_region']] = {'cases': 1, \
-                            'latitude': float(row['latitude']), \
-                                'longitude': float(row['longitude'])}
-                    else:
-                        d[row['health_region']]['cases'] += 1
-            except ValueError:  # no longitude nor latitude
-                pass
-    return d
+    if ageRange is None or gd[key]['ageRange'] == ageRange:
+        if gender is None or gd[key]['gender'] == gender:
+            return True
+    return False
 
 if __name__ == '__main__':
-    d = load_dict_by_age('server\COVID19_case_details.csv', '80+')
-    for key in d:
-        print(f'{key}: {d[key]}')
+    t = query(gender='Male', ageRange='80+')
+
+    for key in t:
+        print(f'{key}: {t[key]}')
